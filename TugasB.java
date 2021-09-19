6c14daeae407d9ba6db8c0fd53914ab3b9e13909
AllIHad <90444763+AllIHad@users.noreply.github.com>
2021-09-19 23:12:08 +0700

    Add files via upload

---
+++ TugasB.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class TugasB {
+    public static void main(String[] args){
+        Scanner bil = new Scanner(System.in);
+        int a, b;
+        a = bil.nextInt();
+        b = bil.nextInt();
+        System.out.println(a + b);
+    }
+}
---
+++ TugasC.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class TugasC {
+    public static void main(String[] args){
+        Scanner katakata = new Scanner(System.in);
+        String a;
+        a = katakata.nextLine();
+        System.out.println(a);
+    }
+}
